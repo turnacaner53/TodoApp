baf7498872987d597f1ac230f9a41b566ac44cd6
Caner Turna <turnacaner53@gmail.com>
2022-02-20 07:20:55 +0300

    Add project files.

---
+++ src/TodoApp.Application/TodoAppService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Entities;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace TodoApp;
+
+public class TodoAppService : ApplicationService, ITodoAppService
+{
+    private readonly IRepository<TodoItem, Guid> todoItemRepository;
+
+    public TodoAppService(IRepository<TodoItem, Guid> todoItemRepository)
+    {
+        this.todoItemRepository = todoItemRepository;
+    }
+
+    public async Task<TodoItemDto> CreateAsync(string text)
+    {
+        var todoItem = await todoItemRepository.InsertAsync(new TodoItem { Text = text });
+
+        return new TodoItemDto
+        {
+            Id = todoItem.Id,
+            Text = todoItem.Text,
+        };
+    }
+
+    public async Task DeleteAysnc(Guid id)
+    {
+        await todoItemRepository.DeleteAsync(id);
+    }
+
+    public async Task<List<TodoItemDto>> GetListAsync()
+    {
+        var items = await todoItemRepository.GetListAsync();
+        return items
+            .Select(i => new TodoItemDto
+            {
+                Id = i.Id,
+                Text = i.Text
+            }).ToList();
+    }
+}
---
+++ src/TodoApp.Blazor/Pages/Index.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TodoApp.Blazor.Pages;
+
+public partial class Index
+{
+    [Inject]
+    private ITodoAppService TodoAppService { get; set; }
+
+    private List<TodoItemDto> TodoItems { get; set; } = new List<TodoItemDto>();
+    private string NewTodoText { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        TodoItems = await TodoAppService.GetListAsync();
+    }
+
+    private async Task Create()
+    {
+        var result = await TodoAppService.CreateAsync(NewTodoText);
+        TodoItems.Add(result);
+        NewTodoText = null;
+    }
+
+    private async Task Delete(TodoItemDto todoItem)
+    {
+        await TodoAppService.DeleteAysnc(todoItem.Id);
+        await Notify.Info("Todo item deleted = " + todoItem.Text);
+        TodoItems.Remove(todoItem);
+    }
+}
